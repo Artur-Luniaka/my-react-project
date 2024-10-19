89046379fa77177202689658e6bf94a488c12aa8
Artur Luniaka <artur01049718@gmail.com>
2024-10-19 17:27:36 +0100

    continue practice by LMS lessons

---
+++ src/components/BookList.jsx
@@ -0,0 +1,29 @@
+// export const favouriteBooks = [
+//   { id: "id-1", name: "JS for beginners" },
+//   { id: "id-2", name: "React basics" },
+//   { id: "id-3", name: "React Router overview" }
+// ];
+
+// export const BookList = ({ books }) => {
+//   return (
+// 	<ul>
+// 	  {books.map((book) => {
+// 		return <li key={book.id}>{book.name}</li>;
+// 	  })}
+// 	</ul>
+//   );
+// };
+
+// export const favouriteBooks = [
+//   { name: "JS for beginners" },
+//   { name: "React basics" },
+//   { name: "React Router overview" }
+// ];
+
+// export const BookList = ({ books }) => (
+//   <ul>
+// 	{books.map((book, index) => {
+// 	  return <li key={index}>{book.name}</li>;
+// 	})}
+//   </ul>
+// );
