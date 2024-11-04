db4222f8545808d3b2d973c9101052cfae956eb0
Artur Luniaka <artur01049718@gmail.com>
2024-11-04 09:04:57 +0100

    practice by LMS lessons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import LangSwitcher from './LangSwitcher/LangSwitcher';
-import FormLogin from './FormLogin/FormLogin';
-import SearchBar from './SearchBar/SearchBar';
-import FeedbackForm from './FeedbackForm/FeedbackForm';
+// import LangSwitcher from './LangSwitcher/LangSwitcher';
+// import FormLogin from './FormLogin/FormLogin';
+// import SearchBar from './SearchBar/SearchBar';
+// import FeedbackForm from './FeedbackForm/FeedbackForm';
+import axios from 'axios';
 // import Product from './Product';
 // import { BookList, favouriteBooks } from './BookList';
 // import { Alert } from './Alert';
@@ -471,8 +472,149 @@ import FeedbackForm from './FeedbackForm/FeedbackForm';
 
 // Create Form With Formik
 
-const App = () => {
-  return <FeedbackForm />;
-};
+// const App = () => {
+//   return <FeedbackForm />;
+// };
 
+// export default App;
+
+// Request on server / Save response to state / Using load indicator / SetError
+
+// const ArticleList = ({ items }) => (
+//   <ul>
+//     {items.map(({ objectID, url, title }) => (
+//       <li key={objectID}>
+//         <a href={url} target="_blank" rel="noreferrer noopener">
+//           {title}
+//         </a>
+//       </li>
+//     ))}
+//   </ul>
+// );
+
+// const App = () => {
+//   const [articles, setArticles] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(false);
+
+//   useEffect(() => {
+//     async function fetchArticles() {
+//       try {
+//         // 1. Встановлюємо індикатор в true перед запитом
+//         setLoading(true);
+//         const response = await axios.get('https://hn.algolia.com/api/v1/search?query=react');
+//         setArticles(response.data.hits);
+//       } catch (error) {
+//         // Встановлюємо стан error в true
+//         setError(true);
+//       } finally {
+//         // 2. Встановлюємо індикатор в false після запиту
+//         setLoading(false);
+//       }
+//     }
+
+//     fetchArticles();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Latest articles</h1>
+//       {loading && <p>Loading data, please wait...</p>}
+//       {error && <p>Whoops, something went wrong! Please try reloading this page!</p>}
+//       {articles.length > 0 && <ArticleList items={articles} />}
+//     </div>
+//   );
+// };
+
+// Using export file with function for request
+// 1. Імпортуємо HTTP-функцію
+// import { fetchArticlesWithTopic } from '../articles-api.js';
+
+// const ArticleList = ({ items }) => (
+//   <ul>
+//     {items.map(({ objectID, url, title }) => (
+//       <li key={objectID}>
+//         <a href={url} target="_blank" rel="noreferrer noopener">
+//           {title}
+//         </a>
+//       </li>
+//     ))}
+//   </ul>
+// );
+
+// const App = () => {
+//   const [articles, setArticles] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(false);
+
+//   useEffect(() => {
+//     async function fetchArticles() {
+//       try {
+//         setLoading(true);
+//         // 2. Використовуємо HTTP-функцію
+//         const data = await fetchArticlesWithTopic('react');
+//         setArticles(data);
+//       } catch (error) {
+//         setError(true);
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+
+//     fetchArticles();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Latest articles</h1>
+//       {loading && <p>Loading data, please wait...</p>}
+//       {error && <p>Whoops, something went wrong! Please try reloading this page!</p>}
+//       {articles.length > 0 && <ArticleList items={articles} />}
+//     </div>
+//   );
+// };
+
+// Request by user (without useEffect)
+import { fetchArticlesWithTopic } from '../articles-api.js';
+import SearchForm from './SearchForm/SearchForm.jsx';
+
+const ArticleList = ({ items }) => (
+  <ul>
+    {items.map(({ objectID, url, title }) => (
+      <li key={objectID}>
+        <a href={url} target="_blank" rel="noreferrer noopener">
+          {title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
+
+const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleSearch = async topic => {
+    try {
+      setArticles([]);
+      setError(false);
+      setLoading(true);
+      const data = await fetchArticlesWithTopic(topic);
+      setArticles(data);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <div>
+      <SearchForm onSearch={handleSearch} />
+      {loading && <p>Loading data, please wait...</p>}
+      {error && <p>Whoops, something went wrong! Please try reloading this page!</p>}
+      {articles.length > 0 && <ArticleList items={articles} />}
+    </div>
+  );
+};
 export default App;
